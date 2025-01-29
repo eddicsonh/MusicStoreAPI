bf377a57853a87e8ed5afe52367406c7dd3fe43b
Eddicson Hernandez <eddicson.hernandez@globallogic.com>
2025-01-29 12:04:38 -0400

    Add a concert class and refactor entities to have a EntityBase class.

---
+++ MusicStore.Entities/Concert.cs
@@ -0,0 +1,18 @@
+﻿namespace MusicStore.Entities
+{
+    public class Concert : EntityBase
+    {
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string Place { get; set; } = default!;
+        public decimal UnitePrice { get; set; }
+        public DateTime DateEvent { get; set; }
+        public int TicketQuantity { get; set; }
+        public bool Finalized { get; set; }
+        public int GenreId { get; set; }
+        public string? ImageUrl { get; set; }
+
+        //Navigation properties
+        public Genre Genre { get; set; } = default!;
+    }
+}
